c1753d8c5c026e1dae449b4c914cbc0bef2f817d
Villan11223344 <sukoonfahad1@gmail.com>
2024-06-25 14:47:39 +0500

    Update Offline.py

--- Offline.py
+++ Offline.py
@@ -4,7 +4,6 @@ os.system('git pull --quiet 2>/dev/null')
 mrMani = platform.architecture()[0]
 if mrMani== '64bit':
  os.system('xdg-open https://github.com/Villan11223344/hum> OFFLINE64")
-
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit');time.sleep(2)
 elif mrMani== '32bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is Not Supported');time.sleep(2);exit()
