8816969fa6155985244f46514780a72032848d33
Villan11223344 <sukoonfahad1@gmail.com>
2024-06-25 11:44:12 +0500

    Add files via upload

---
+++ Offline.py
@@ -0,0 +1,13 @@
+import os, platform, time, sys
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+os.system('xdg-open https://chat.whatsapp.com/KThLuumIszgD0UCJz3T33C')
+mrMani = platform.architecture()[0]
+if mrMani== '64bit':
+ os.system("curl -L https://github.com/Villan11223344/Mani> OFFLINE64")
+
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit');time.sleep(2)
+ os.system('chmod 777 OFFLINE64 && ./OFFLINE64')
+elif mrMani== '32bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is Not Supported');time.sleep(2);exit()
+ 
